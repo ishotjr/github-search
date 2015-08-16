f0bf4d5724aaa9991a5087b85d93f821211209be
ishotjr <github@ishotjr.com>
2015-08-16 18:31:49 -0400

    :octocat: :lock_with_ink_pen: Add Git credential helper, main frame

--- search.py
+++ search.py
@@ -4,8 +4,38 @@ import os, subprocess
 import wx
 from agithub import Github
 
+GITHUB_HOST = 'github.com'
+def git_credentials():
+	os.environ['GIT_ASKPASS'] = 'true'
+	p = subprocess.Popen(['git', 'credential', 'fill'],
+		stdout=subprocess.PIPE,
+		stdin=subprocess.PIPE)
+	stdout,_ = p.communicate('host={}\n\n'.format(GITHUB_HOST))
+
+	creds = {}
+	for line in stdout.split('\n')[:-1:]:
+		k,v = line.split('=')
+		creds[k] = v
+	return creds
+
 class SearchFrame(wx.Frame):
-	pass
+	def __init__(self, *args, **kwargs):
+		kwargs.setdefault('size', (600,500))
+		wx.Frame.__init__(self, *args, **kwargs)
+
+		self.credentials = {}
+		self.orgs = []
+
+		#self.create_controls()
+		#self.do_layout()
+
+		# Try to pre-load credentials from Git's cache
+		self.credentials = git_credentials()
+		#if self.test_credentials():
+		#	self.switch_to_search_panel()
+
+		self.SetTitle('GitHub Issue Search')
+		self.Show()
 
 if __name__ == '__main__':
 	app = wx.App()
