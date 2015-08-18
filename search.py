222071bbae5a67c3b5cd7865b824f214a2167c7b
ishotjr <github@ishotjr.com>
2015-08-17 20:39:54 -0400

    :lock_with_ink_pen: :black_square_button: Add LoginPanel

--- search.py
+++ search.py
@@ -18,6 +18,7 @@ def git_credentials():
 		creds[k] = v
 	return creds
 
+
 class SearchFrame(wx.Frame):
 	def __init__(self, *args, **kwargs):
 		kwargs.setdefault('size', (600,500))
@@ -82,6 +83,58 @@ class SearchFrame(wx.Frame):
 										credentials=self.credentials)
 		self.sizer.Add(self.search_panel, 1, flag=wx.EXPAND | wx.ALL, border=10)
 		self.sizer.Layout()
+
+
+class LoginPanel(wx.Panel):
+	def __init__(self, *args, **kwargs):
+		self.callback = kwargs.pop('onlogin', None)
+		wx.Panel.__init__(self, *args, **kwargs)
+
+		self.create_controls()
+		self.do_layout()
+
+	def create_controls(self):
+		self.userLabel = wx.StaticText(self, label='Username:')
+		self.userBox = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
+		self.passLabel = wx.StaticText(self, label='Password (or token):')
+		self.passBox = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
+		self.login = wx.Button(self, label='Log in')
+		self.error = wx.StaticText(self, label='')
+		self.error.SetForegroundColour((200,0,0))
+
+		# bind events
+		self.login.Bind(wx.EVT_BUTTON, self.do_login)
+		self.userBox.Bind(wx.EVT_TEXT_ENTER, self.do_login)		
+		self.passBox.Bind(wx.EVT_TEXT_ENTER, self.do_login)
+
+	def do_layout(self):
+		# grid arrangement for controls
+		grid = wx.GridBagSizer(3,3)
+		grid.Add(self.userLabel, pos=(0,0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
+		grid.Add(self.userBox, pos=(0,1), flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
+		grid.Add(self.passLabel, pos=(1,0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
+		grid.Add(self.passBox, pos=(1,1), flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
+		grid.Add(self.login, pos=(2,0), flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
+		grid.Add(self.error, pos=(3,0), flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
+		grid.AddGrowableCol(1)
+
+		# center the grid vertically
+		vbox = wx.BoxSizer(wx.VERTICAL)
+		vbox.Add((0,0), 1)
+		vbox.Add(grid, 0, wx.EXPAND)
+		vbox.Add((0,0), 2)
+		self.SetSizer(vbox)
+
+	def do_login(self, _):
+		u = self.userBox.GetValue()
+		p = self.passBox.GetValue()
+		g = Github(u, p)
+		status,data = g.issues.get()
+		if status != 200:
+			self.error.SetLabel('ERROR: ' + data['message'])
+		elif callable(self.callback):
+			self.callback(u, p)
+
 
 if __name__ == '__main__':
 	app = wx.App()
